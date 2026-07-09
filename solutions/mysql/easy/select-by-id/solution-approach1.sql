6bd5b290ebbca24f6af571a89eaa5c71ef13aeaf
KAVIBHARATHAN <kavibharathank@gmail.com>
2026-07-09 10:53:30 +0530

    ✅ Select By ID [mysql] — approach1

---
+++ solutions/mysql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-09, 10:53 a.m.
+-- ──────────────────────────────────────────────────
+
+SELECT * from CITY
+where id='1661';
