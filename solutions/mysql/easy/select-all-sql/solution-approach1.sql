74156f28550a6e529831046bb35e78897549a87a
KAVIBHARATHAN <kavibharathank@gmail.com>
2026-07-09 10:53:07 +0530

    ✅ Select All [mysql] — approach1

---
+++ solutions/mysql/easy/select-all-sql/solution-approach1.sql
@@ -0,0 +1,11 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Select All
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-09, 10:53 a.m.
+-- ──────────────────────────────────────────────────
+
+select id,name,countrycode,district,population from city;
